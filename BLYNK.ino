609e774b952294f2a1ec04fa847cb957150b32de
Willy Riyadi <86903170+Wriyadi@users.noreply.github.com>
2023-05-12 11:56:34 +0700

    BLYNK.ino

--- BLYNK.ino
+++ BLYNK.ino
@@ -1,8 +1,5 @@
-#define BLYNK_TEMPLATE_ID           "TMPLnCxxx"
-#define BLYNK_DEVICE_NAME           "Quickstart Device"
-#define BLYNK_AUTH_TOKEN            "HOC1HLpEiTjRxxx"
+#define BLYNK_AUTH_TOKEN            "HOC1HLpxxx"
 #define BLYNK_PRINT Serial
-#include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
@@ -19,12 +16,12 @@ BLYNK_WRITE(V1) // Executes when the value of virtual pin 1 changes
   if(param.asInt() == 1)
   {
     // execute this code if the switch widget is now ON
-    digitalWrite(16,HIGH);  // Set digital pin 2 HIGH
+    digitalWrite(D1,HIGH);  // Set digital pin 2 HIGH
   }
   else
   {
     // execute this code if the switch widget is now OFF
-    digitalWrite(16,LOW);  // Set digital pin 2 LOW    
+    digitalWrite(D1,LOW);  // Set digital pin 2 LOW    
   }
 }
 
@@ -33,12 +30,12 @@ BLYNK_WRITE(V2) // Executes when the value of virtual pin 2 changes
   if(param.asInt() == 1)
   {
     // execute this code if the switch widget is now ON
-    digitalWrite(5,HIGH);  // Set digital pin 2 HIGH
+    digitalWrite(D2,HIGH);  // Set digital pin 2 HIGH
   }
   else
   {
     // execute this code if the switch widget is now OFF
-    digitalWrite(5,LOW);  // Set digital pin 2 LOW    
+    digitalWrite(D2,LOW);  // Set digital pin 2 LOW    
   }
 }
 
@@ -47,12 +44,12 @@ BLYNK_WRITE(V3) // Executes when the value of virtual pin 3 changes
   if(param.asInt() == 1)
   {
     // execute this code if the switch widget is now ON
-    digitalWrite(4,HIGH);  // Set digital pin 2 HIGH
+    digitalWrite(D3,HIGH);  // Set digital pin 2 HIGH
   }
   else
   {
     // execute this code if the switch widget is now OFF
-    digitalWrite(4,LOW);  // Set digital pin 2 LOW    
+    digitalWrite(D3,LOW);  // Set digital pin 2 LOW    
   }
 }
 
@@ -73,9 +70,9 @@ void setup()
   //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
   //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 
-  pinMode(16, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
   }
 
 void loop()
