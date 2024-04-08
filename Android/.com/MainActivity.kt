accbca1b116838eacc150852b65c33e5df989103
Willy Riyadi <86903170+Wriyadi@users.noreply.github.com>
2024-04-08 14:15:54 +0700

    Update MainActivity.kt

--- Android/.com/MainActivity.kt
+++ Android/.com/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.firebase_iot
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -26,6 +28,13 @@ class MainActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
 
+        val email = intent.getStringExtra("email")
+        email?.let {
+            binding.uid.text = it.toEditable()
+        } ?: run {
+            Log.e("MainActivity", "Email extra is null")
+            Toast.makeText(this, "Email extra is null", Toast.LENGTH_SHORT).show()
+        }
         // Set onClickListeners for buttons
         binding.button1.setOnClickListener { setData(binding.button1, "16") }
         binding.button2.setOnClickListener { setData(binding.button2, "5") }
@@ -33,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         binding.button4.setOnClickListener { setData(binding.button4, "0") }
         binding.button5.setOnClickListener { setData(binding.button5, "2") }
         binding.button6.setOnClickListener { setData(binding.button6, "14") }
-        binding.getButton.setOnClickListener { getTemperature() } // Set click listener for getButton
+        binding.getButton.setOnClickListener { getTempHum() } // Set click listener for getButton
         binding.logoutButton.setOnClickListener { logoutUser() } // Set click listener for logoutButton
     }
 
@@ -60,21 +69,43 @@ class MainActivity : AppCompatActivity() {
         button.text = if (value == 1) "ON" else "OFF"
     }
 
-    private fun getTemperature() {
+    private fun getTempHum() {
         val temperatureChildId = "temp"
-        database.child(temperatureChildId).get().addOnSuccessListener {
-            if (it.exists()) {
-                val temp:Float = it.value.toString().toFloat()
-                Toast.makeText(this@MainActivity, "Success", Toast.LENGTH_SHORT).show()
-                binding.temperature.setText(temp.toString())
+        val humidityChildId = "hum"
+
+        database.child(temperatureChildId).get().addOnCompleteListener { temperatureTask ->
+            if (temperatureTask.isSuccessful) {
+                val temperatureSnapshot = temperatureTask.result
+                if (temperatureSnapshot != null && temperatureSnapshot.exists()) {
+                    val temperature = temperatureSnapshot.value.toString().toFloat()
+                    val temperatureString = "$temperature °C"
+                    this.binding.temperature.setText(temperatureString)
+                    Toast.makeText(this@MainActivity, "Temperature data retrieved successfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Temperature data not available", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "Failed to retrieve temperature data", Toast.LENGTH_SHORT).show()
             }
-            else {
-                Toast.makeText(this@MainActivity, "Temperature data not available", Toast.LENGTH_SHORT).show()
+        }
+
+        database.child(humidityChildId).get().addOnCompleteListener { humidityTask ->
+            if (humidityTask.isSuccessful) {
+                val humiditySnapshot = humidityTask.result
+                if (humiditySnapshot != null && humiditySnapshot.exists()) {
+                    val humidity = humiditySnapshot.value.toString().toFloat()
+                    val humidityString = "$humidity %"
+                    this.binding.humidity.setText(humidityString)
+                    Toast.makeText(this@MainActivity, "Humidity data retrieved successfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Humidity data not available", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "Failed to retrieve humidity data", Toast.LENGTH_SHORT).show()
             }
-        }.addOnFailureListener {
-            Toast.makeText(this@MainActivity, "Failed to retrieve temperature data", Toast.LENGTH_SHORT).show()
         }
     }
+
 
     private fun logoutUser() {
         // Sign out the user
@@ -84,3 +115,5 @@ class MainActivity : AppCompatActivity() {
         finish() // Finish MainActivity
     }
 }
+
+private fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
