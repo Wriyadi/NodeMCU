283dfbf072ed0da774f42b85502a8b68b8a01256
Willy Riyadi <86903170+Wriyadi@users.noreply.github.com>
2024-05-20 11:39:46 +0700

    Update LocalHost2.ino

--- LocalHost2.ino
+++ LocalHost2.ino
@@ -1,27 +1,36 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <DHT.h>
 
 // Replace with your network credentials
 const char* ssid     = "SSID";
 const char* password = "PASSWORD";
 
 // Set web server port number to 80
-ESP8266WebServer server(80);      //Menyatakan Webserver pada port 80
+ESP8266WebServer server(80);
 
-// Variable to store the HTTP request
+// DHT Sensor setup
+#define DHTPIN 2     // D4 pin
+#define DHTTYPE DHT22   // DHT 22 (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+float temperature;
+float humidity;
+
+// Variable to store the HTML web page
 String webpage;
 
-#define LED1 16  // Pemilihan Pin D0
-#define LED2 5    // Pemilihan Pin D1
-#define LED3 4    // Pemilihan Pin D2
-#define LED4 0  // Pemilihan Pin D3
-#define LED5 2  // Pemilihan Pin D4
-#define LED6 14    // Pemilihan Pin D5
-#define LED7 12   // Pemilihan Pin D6
-#define LED8 13    // Pemilihan Pin D7
-#define LED9 15   // Pemilihan Pin D8
+#define LED1 16  // D0
+#define LED2 5   // D1
+#define LED3 4   // D2
+#define LED4 0   // D3
+#define LED5 2   // D4
+#define LED6 14  // D5
+#define LED7 12  // D6
+#define LED8 13  // D7
+#define LED9 15  // D8
 
-void setup() {  //Pengaturan Pin
+void setup() {
   Serial.begin(115200);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -33,140 +42,135 @@ void setup() {  //Pengaturan Pin
   pinMode(LED8, OUTPUT);
   pinMode(LED9, OUTPUT);
 
-  // Connect ke WiFi
+  dht.begin();
+
+  // Connect to Wi-Fi
   Serial.println();
   Serial.print("Configuring access point...");
 
-  // Mengatur WiFi 
-  WiFi.mode(WIFI_AP);                      // Mode Station
-  WiFi.begin(ssid, password);               // Mencocokan SSID dan Password
+  // Set up Wi-Fi
+  WiFi.mode(WIFI_AP);
+  WiFi.begin(ssid, password);
 
-  // Print status Connect 
-  Serial.println("IP address: ");
+  // Print status
+  Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
 
-  // Isi dari Webpage 
-  webpage += "&lt;h1&gt; Web Control LED ESP8266&lt;/h1&gt;";
-  webpage += "&lt;wp-p&gt;LED 1 : ";
-  webpage += "&lt;a href=\"LED1ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED1OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 2 : ";
-  webpage += "&lt;a href=\"LED2ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED2OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 3 : ";
-  webpage += "&lt;a href=\"LED3ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED3OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 4 : ";
-  webpage += "&lt;a href=\"LED4ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED4OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 5 : ";
-  webpage += "&lt;a href=\"LED5ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED5OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 6 : ";
-  webpage += "&lt;a href=\"LED6ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED6OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 7 : ";
-  webpage += "&lt;a href=\"LED7ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED7OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 8 : ";
-  webpage += "&lt;a href=\"LED8ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED8OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;&lt;wp-br&gt;";
-  webpage += "&lt;wp-p&gt;LED 9 : ";
-  webpage += "&lt;a href=\"LED9ON\"\"&gt;&lt;button&gt;ON&lt;/button&gt;&lt;/a&gt;&lt;a href=\"LED9OFF\"\"&gt;&lt;button&gt;OFF&lt;/button&gt;&lt;/a&gt;&lt;/wp-p&gt;";
+  // Set up webpage content
+  webpage += "<h1>Web Control LED ESP8266</h1>";
+  webpage += "<p>LED 1: <a href=\"LED1ON\"><button>ON</button></a><a href=\"LED1OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 2: <a href=\"LED2ON\"><button>ON</button></a><a href=\"LED2OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 3: <a href=\"LED3ON\"><button>ON</button></a><a href=\"LED3OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 4: <a href=\"LED4ON\"><button>ON</button></a><a href=\"LED4OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 5: <a href=\"LED5ON\"><button>ON</button></a><a href=\"LED5OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 6: <a href=\"LED6ON\"><button>ON</button></a><a href=\"LED6OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 7: <a href=\"LED7ON\"><button>ON</button></a><a href=\"LED7OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 8: <a href=\"LED8ON\"><button>ON</button></a><a href=\"LED8OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>LED 9: <a href=\"LED9ON\"><button>ON</button></a><a href=\"LED9OFF\"><button>OFF</button></a></p>";
+  webpage += "<p>Temperature: <span id=\"temperature\">--</span> &deg;C</p>";
+  webpage += "<p>Humidity: <span id=\"humidity\">--</span> %</p>";
+  webpage += "<script>setInterval(function() {fetch('/data').then(response => response.json()).then(data => {document.getElementById('temperature').innerText = data.temperature;document.getElementById('humidity').innerText = data.humidity;});}, 2000);</script>";
 
-  // Membuat file webpage 
+  // Configure web server
   server.on("/", []() {
     server.send(200, "text/html", webpage);
   });
 
-  // Bagian ini untuk merespon perintah yang masuk 
+  server.on("/data", []() {
+    String data = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
+    server.send(200, "application/json", data);
+  });
+
+  // LED control handlers
   server.on("/LED1ON", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED1, HIGH);
-    delay(500);
-  });
-  server.on("/LED2ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED2, HIGH);
-    delay(500);
-  });
-  server.on("/LED3ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED3, HIGH);
-    delay(500);
-  });
-    server.on("/LED4ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED4, HIGH);
-    delay(500);
-  });
-  server.on("/LED5ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED5, HIGH);
-    delay(500);
-  });
-  server.on("/LED6ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED6, HIGH);
-    delay(500);
-  });
-    server.on("/LED7ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED7, HIGH);
-    delay(500);
-  });
-  server.on("/LED8ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED8, HIGH);
-    delay(500);
-  });
-  server.on("/LED9ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED9, HIGH);
-    delay(500);
   });
   server.on("/LED1OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED1, LOW);
-    delay(500);
+  });
+  server.on("/LED2ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED2, HIGH);
   });
   server.on("/LED2OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED2, LOW);
-    delay(500);
+  });
+  server.on("/LED3ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED3, HIGH);
   });
   server.on("/LED3OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED3, LOW);
-    delay(500);
   });
-    server.on("/LED4OFF", []() {
+  server.on("/LED4ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED4, HIGH);
+  });
+  server.on("/LED4OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED4, LOW);
-    delay(500);
+  });
+  server.on("/LED5ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED5, HIGH);
   });
   server.on("/LED5OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED5, LOW);
-    delay(500);
+  });
+  server.on("/LED6ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED6, HIGH);
   });
   server.on("/LED6OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED6, LOW);
-    delay(500);
   });
-    server.on("/LED7OFF", []() {
+  server.on("/LED7ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED7, HIGH);
+  });
+  server.on("/LED7OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED7, LOW);
-    delay(500);
+  });
+  server.on("/LED8ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED8, HIGH);
   });
   server.on("/LED8OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED8, LOW);
-    delay(500);
+  });
+  server.on("/LED9ON", []() {
+    server.send(200, "text/html", webpage);
+    digitalWrite(LED9, HIGH);
   });
   server.on("/LED9OFF", []() {
     server.send(200, "text/html", webpage);
     digitalWrite(LED9, LOW);
-    delay(500);
   });
 
   server.begin();
-  Serial.println("Server dijalankan");
+  Serial.println("Server started");
 }
 
-void loop() {  //Perulangan Program
+void loop() {
   server.handleClient();
+
+  // Read the temperature and humidity
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+
+  // Print the sensor data to the Serial Monitor
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" *C ");
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
 }
