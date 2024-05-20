8751896ddb3d9d7fe0fb14d62b34259c3bada20b
Willy Riyadi <86903170+Wriyadi@users.noreply.github.com>
2024-05-20 11:50:55 +0700

    Update LocalHost.ino

--- LocalHost.ino
+++ LocalHost.ino
@@ -1,177 +1,278 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <DHT.h>
+include <ESP8266WiFi.h>
 
 // Replace with your network credentials
 const char* ssid     = "SSID";
 const char* password = "PASSWORD";
 
 // Set web server port number to 80
-ESP8266WebServer server(80);
+WiFiServer server(80);
 
-// DHT Sensor setup
-#define DHTPIN 2     // D4 pin
-#define DHTTYPE DHT22   // DHT 22 (AM2302)
-DHT dht(DHTPIN, DHTTYPE);
+// Variable to store the HTTP request
+String header;
 
-float temperature;
-float humidity;
+// Auxiliar variables to store the current output state
+String output8State = "off";
+String output7State = "off";
+String output6State = "off";
+String output5State = "off";
+String output4State = "off";
+String output3State = "off";
+String output2State = "off";
+String output1State = "off";
 
-// Variable to store the HTML web page
-String webpage;
+// Assign output variables to GPIO pins
+const int output8 = 13;
+const int output7 = 12;
+const int output6 = 14;
+const int output5 = 2;
+const int output4 = 0;
+const int output3 = 4;
+const int output2 = 5;
+const int output1 = 16;
 
-#define LED1 16  // D0
-#define LED2 5   // D1
-#define LED3 4   // D2
-#define LED4 0   // D3
-#define LED5 2   // D4
-#define LED6 14  // D5
-#define LED7 12  // D6
-#define LED8 13  // D7
-#define LED9 15  // D8
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  pinMode(LED6, OUTPUT);
-  pinMode(LED7, OUTPUT);
-  pinMode(LED8, OUTPUT);
-  pinMode(LED9, OUTPUT);
+  // Initialize the output variables as outputs
+  pinMode(output8, OUTPUT);
+  pinMode(output7, OUTPUT);
+  pinMode(output6, OUTPUT);
+  pinMode(output5, OUTPUT);
+  pinMode(output4, OUTPUT);
+  pinMode(output3, OUTPUT);
+  pinMode(output2, OUTPUT);
+  pinMode(output1, OUTPUT);
 
-  // Initialize DHT sensor
-  dht.begin();
+  // Set outputs to LOW
+  digitalWrite(output8, LOW);
+  digitalWrite(output7, LOW);
+  digitalWrite(output6, LOW);
+  digitalWrite(output5, LOW);
+  digitalWrite(output4, LOW);
+  digitalWrite(output3, LOW);
+  digitalWrite(output2, LOW);
+  digitalWrite(output1, LOW);
 
-  // Connect to Wi-Fi
-  Serial.println();
-  Serial.print("Configuring access point...");
-
-  // Set up Wi-Fi
-  WiFi.mode(WIFI_AP);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
-
-  // Print status
-  Serial.print("IP address: ");
-  Serial.println(WiFi.softAPIP());
-
-  // Set up webpage content
-  webpage += "<h1>Web Control LED ESP8266</h1>";
-  webpage += "<p>LED 1: <a href=\"LED1ON\"><button>ON</button></a><a href=\"LED1OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 2: <a href=\"LED2ON\"><button>ON</button></a><a href=\"LED2OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 3: <a href=\"LED3ON\"><button>ON</button></a><a href=\"LED3OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 4: <a href=\"LED4ON\"><button>ON</button></a><a href=\"LED4OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 5: <a href=\"LED5ON\"><button>ON</button></a><a href=\"LED5OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 6: <a href=\"LED6ON\"><button>ON</button></a><a href=\"LED6OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 7: <a href=\"LED7ON\"><button>ON</button></a><a href=\"LED7OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 8: <a href=\"LED8ON\"><button>ON</button></a><a href=\"LED8OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>LED 9: <a href=\"LED9ON\"><button>ON</button></a><a href=\"LED9OFF\"><button>OFF</button></a></p>";
-  webpage += "<p>Temperature: <span id=\"temperature\">--</span> &deg;C</p>";
-  webpage += "<p>Humidity: <span id=\"humidity\">--</span> %</p>";
-  webpage += "<script>setInterval(function() {fetch('/data').then(response => response.json()).then(data => {document.getElementById('temperature').innerText = data.temperature;document.getElementById('humidity').innerText = data.humidity;});}, 2000);</script>";
-
-  // Configure web server
-  server.on("/", []() {
-    server.send(200, "text/html", webpage);
-  });
-
-  server.on("/data", []() {
-    String data = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
-    server.send(200, "application/json", data);
-  });
-
-  // LED control handlers
-  server.on("/LED1ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED1, HIGH);
-  });
-  server.on("/LED1OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED1, LOW);
-  });
-  server.on("/LED2ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED2, HIGH);
-  });
-  server.on("/LED2OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED2, LOW);
-  });
-  server.on("/LED3ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED3, HIGH);
-  });
-  server.on("/LED3OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED3, LOW);
-  });
-  server.on("/LED4ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED4, HIGH);
-  });
-  server.on("/LED4OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED4, LOW);
-  });
-  server.on("/LED5ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED5, HIGH);
-  });
-  server.on("/LED5OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED5, LOW);
-  });
-  server.on("/LED6ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED6, HIGH);
-  });
-  server.on("/LED6OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED6, LOW);
-  });
-  server.on("/LED7ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED7, HIGH);
-  });
-  server.on("/LED7OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED7, LOW);
-  });
-  server.on("/LED8ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED8, HIGH);
-  });
-  server.on("/LED8OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED8, LOW);
-  });
-  server.on("/LED9ON", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED9, HIGH);
-  });
-  server.on("/LED9OFF", []() {
-    server.send(200, "text/html", webpage);
-    digitalWrite(LED9, LOW);
-  });
-
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
   server.begin();
-  Serial.println("Server started");
 }
 
-void loop() {
-  server.handleClient();
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
-  // Read the temperature and humidity
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
 
-  // Print the sensor data to the Serial Monitor
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print(" *C ");
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /1/on") >= 0) {
+              Serial.println("GPIO 16 on");
+              output1State = "on";
+              digitalWrite(output1, HIGH);
+            } else if (header.indexOf("GET /1/off") >= 0) {
+              Serial.println("GPIO 16 off");
+              output1State = "off";
+              digitalWrite(output1, LOW);
+            } else if (header.indexOf("GET /2/on") >= 0) {
+              Serial.println("GPIO 5 on");
+              output2State = "on";
+              digitalWrite(output2, HIGH);
+            } else if (header.indexOf("GET /2/off") >= 0) {
+              Serial.println("GPIO 5 off");
+              output2State = "off";
+              digitalWrite(output2, LOW);
+            } else if (header.indexOf("GET /3/on") >= 0) {
+              Serial.println("GPIO 4 on");
+              output3State = "on";
+              digitalWrite(output3, HIGH);
+            } else if (header.indexOf("GET /3/off") >= 0) {
+              Serial.println("GPIO 4 off");
+              output3State = "off";
+              digitalWrite(output3, LOW);
+            } else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("GPIO 0 on");
+              output4State = "on";
+              digitalWrite(output4, HIGH);
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("GPIO 0 off");
+              output4State = "off";
+              digitalWrite(output4, LOW);
+            } else if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("GPIO 2 on");
+              output5State = "on";
+              digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("GPIO 2 off");
+              output5State = "off";
+              digitalWrite(output5, LOW);
+            } else if (header.indexOf("GET /6/on") >= 0) {
+              Serial.println("GPIO 14 on");
+              output6State = "on";
+              digitalWrite(output6, HIGH);
+            } else if (header.indexOf("GET /6/off") >= 0) {
+              Serial.println("GPIO 14 off");
+              output6State = "off";
+              digitalWrite(output6, LOW);
+            } else if (header.indexOf("GET /7/on") >= 0) {
+              Serial.println("GPIO 12 on");
+              output7State = "on";
+              digitalWrite(output7, HIGH);
+            } else if (header.indexOf("GET /7/off") >= 0) {
+              Serial.println("GPIO 12 off");
+              output7State = "off";
+              digitalWrite(output7, LOW);
+            } else if (header.indexOf("GET /8/on") >= 0) {
+              Serial.println("GPIO 13 on");
+              output8State = "on";
+              digitalWrite(output8, HIGH);
+            } else if (header.indexOf("GET /8/off") >= 0) {
+              Serial.println("GPIO 13 off");
+              output8State = "off";
+              digitalWrite(output8, LOW);
+            } 
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name="viewport" content="width=device-width, initial-scale=1" />");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+
+            // Web Page Heading
+            client.println("<body><h2>NodeMCU ESP8266 WEB Server</h2>");
+            client.println("<body><h3>LED Controller for Pin D0 - D8 </h3>");
+
+            // Display current state, and ON/OFF buttons for Pin D0  
+            client.println("<p>Pin D0 - State " + output1State + "</p>");
+            // If the output1State is off, it displays the ON button       
+            if (output1State=="off") {
+              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            // Display current state, and ON/OFF buttons for Pin D1  
+            client.println("<p>Pin D1 - State " + output2State + "</p>");
+            // If the output2State is off, it displays the ON button       
+            if (output2State=="off") {
+              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+
+            // Display current state, and ON/OFF buttons for Pin D2  
+            client.println("<p>Pin D2 - State " + output3State + "</p>");
+            // If the output3State is off, it displays the ON button       
+            if (output3State=="off") {
+              client.println("<p><a href=\"/3/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/3/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+
+            // Display current state, and ON/OFF buttons for Pin D3  
+            client.println("<p>Pin D3 - State " + output4State + "</p>");
+            // If the output3State is off, it displays the ON button       
+            if (output4State=="off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+
+            // Display current state, and ON/OFF buttons for Pin D4  
+            client.println("<p>Pin D4 - State " + output5State + "</p>");
+            // If the output5State is off, it displays the ON button       
+            if (output5State=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            // Display current state, and ON/OFF buttons for Pin D5  
+            client.println("<p>Pin D5 - State " + output6State + "</p>");
+            // If the output6State is off, it displays the ON button       
+            if (output6State=="off") {
+              client.println("<p><a href=\"/6/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/6/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            // Display current state, and ON/OFF buttons for Pin D6  
+            client.println("<p>Pin D6 - State " + output7State + "</p>");
+            // If the output7State is off, it displays the ON button       
+            if (output7State=="off") {
+              client.println("<p><a href=\"/7/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/7/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            // Display current state, and ON/OFF buttons for Pin D7  
+            client.println("<p>Pin D7 - State " + output8State + "</p>");
+            // If the output8State is off, it displays the ON button       
+            if (output8State=="off") {
+              client.println("<p><a href=\"/8/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/8/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
